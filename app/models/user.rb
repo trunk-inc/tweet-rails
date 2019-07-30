4565e21bf3237f604e7102ec7d7ffa263c1be622
川崎暑士 <kawasaki8910@gmail.com>
2019-07-30 17:58:12 +0900

    Add New Controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :account, presence: true,
+    format: { with: /\A[a-z0-9]+\z/i }
+  validates :username, presence: true
+  validates :introduction, presence: true
+  validates :icon_url, presence: true
+  validates :cover_url, presence: true
 end
